46814988b212ba80d75e6a5f90d9e471aa287b15
John Brainard <DorkyMusician@gmail.com>
2015-08-30 08:59:47 -0700

    Add the views and import them in our activity class

--- app/src/main/java/io/github/johnbrainard/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/io/github/johnbrainard/kotlinphotoframe/MainActivity.kt
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.activity_main.imageView
 
 public class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val imageView = this.imageView
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
